64eaae235114185575937cc477bf0d3ba82a5059
Daniel DiVenere <danieldivenere@gmail.com>
2022-03-31 15:19:24 -0700

    get by ID

--- backend/src/drops/index.ts
+++ backend/src/drops/index.ts
@@ -3,48 +3,47 @@ import { onlyAuthorized } from '../middleware';
 import { Drop } from '../models/Drop';
 
 const dropRouter = express.Router();
-const mainRoute = dropRouter.route('/');
+const allDrops = dropRouter.route('/');
 
-mainRoute.get(onlyAuthorized, async (req, res) => {
-  try {
-    const { currentCoordinates } = req.body;
+allDrops.get(onlyAuthorized, async (req, res) => {
+  const { currentCoordinates } = req.body;
 
-    const drops = await Drop.aggregate([
-      {
-        $geoNear: {
-          near: { type: 'Point', coordinates: currentCoordinates },
-          distanceField: 'dist.calculated',
-          maxDistance: 2,
-          // query: { category: 'Parks' },
-          includeLocs: 'dist.location',
-          spherical: true,
-        },
+  const drops = await Drop.aggregate([
+    {
+      $geoNear: {
+        near: { type: 'Point', coordinates: currentCoordinates },
+        distanceField: 'dist.calculated',
+        maxDistance: 2,
+        // query: { category: 'Parks' },
+        includeLocs: 'dist.location',
+        spherical: true,
       },
-    ])
+    },
+  ]);
 
-    res.json(drops);
-  } catch (err) {
-    res.status(500).send((err as any).message);
-  }
+  res.json(drops);
 });
 
-mainRoute.post(onlyAuthorized, async (req, res) => {
-  try {
-    const { message, type, coordinates } = req.body;
+allDrops.post(onlyAuthorized, async (req, res) => {
+  const { message, type, coordinates } = req.body;
 
-    const drop = await Drop.create({
-      message,
-      type,
-      location: {
-        type: 'Point',
-        coordinates,
-      },
-    });
+  const drop = await Drop.create({
+    message,
+    type,
+    location: {
+      type: 'Point',
+      coordinates,
+    },
+  });
 
-    res.json(drop);
-  } catch (err) {
-    res.status(500).send((err as any).message);
-  }
+  res.status(201).json(drop);
+});
+
+dropRouter.route('/:id').get(onlyAuthorized, async (req, res) => {
+  const { id } = req.params;
+  const drop = await Drop.findById(id);
+
+  res.json(drop)
 });
 
 export default dropRouter;
