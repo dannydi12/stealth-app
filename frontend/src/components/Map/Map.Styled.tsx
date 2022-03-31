40b5a2c7a72a4cb1a2f4a1a65d988c27161dc1e6
pauline-ann <paulinebantayan@gmail.com>
2022-03-31 13:42:54 -0700

    style map

--- frontend/src/components/Map/Map.Styled.tsx
+++ frontend/src/components/Map/Map.Styled.tsx
@@ -2,6 +2,12 @@ import styled from 'styled-components'
 
 const StyledMap = styled.div`
 
+min-height: 100%;
+
+.map-container {
+    height: 100vh;
+    width: 100vw; 
+}
 `
 
 export default StyledMap
