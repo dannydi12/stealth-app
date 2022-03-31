a66a3ede3cdc49c20dc9a19f3e1c37274ddb0c68
Daniel DiVenere <danieldivenere@gmail.com>
2022-03-31 09:49:29 -0700

    backend init

---
+++ backend/src/app.ts
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import CONFIG from '../config'
+
+const app = express()
+
+const morganOption = CONFIG.NODE_ENV === 'production' ? 'tiny' : 'common'
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+app.use((error, req, res, next) => {
+  let response
+  if (CONFIG.NODE_ENV === 'production') {
+    response = {
+      error: {
+        message: 'server error'
+      }
+    }
+  }
+  else {
+    console.log(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).send(response)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+export default app
