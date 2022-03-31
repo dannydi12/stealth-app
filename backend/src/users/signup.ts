3d0da79479c6cc7a859fd3ced1679ca6413e1cb3
Daniel DiVenere <danieldivenere@gmail.com>
2022-03-31 16:54:00 -0700

    fix signup

--- backend/src/users/signup.ts
+++ backend/src/users/signup.ts
@@ -1,21 +1,25 @@
 import express from 'express'
+import { auth } from '../firebase'
 import { onlyAuthorized } from '../middleware'
 import { User } from '../models/User'
 
 const signupRouter = express.Router()
 
-signupRouter.route('/').post(onlyAuthorized, async (req, res) => {
+signupRouter.route('/').post(async (req, res) => {
   try {
     const {
-      _id, username, avatar, signUpLocation,
+      username, avatar, signUpLocation,
     } = req.body
 
-    if (res.user?._id) {
+    const token = req?.headers?.authorization?.split('Bearer ')[1] || ''
+    const decodedToken = await auth.verifyIdToken(token)
+
+    if (decodedToken.uid) {
       throw new Error('Auth error.')
     }
 
     const user = await User.create({
-      _id: res.user?._id,
+      _id: decodedToken.uid,
       username,
       avatar,
       signUpLocation,
