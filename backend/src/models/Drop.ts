b6da0f6408118f8a89a7bde2037a91e77b81b3a0
Daniel DiVenere <danieldivenere@gmail.com>
2022-03-31 14:57:41 -0700

    new types!

---
+++ backend/src/models/Drop.ts
@@ -0,0 +1,33 @@
+import { Schema, model, Document } from 'mongoose'
+import { CommentType } from './Comment';
+import { UserType } from './User';
+
+export interface DropType extends Document {
+  author?: UserType;
+  message: string
+  type: 'message' | 'image' | 'audio'
+  location: {
+    type: string
+    coordinates: number[]
+  }
+  comments: CommentType[]
+}
+
+export const DropSchema = new Schema<DropType>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    message: String,
+    type: { type: String, required: true },
+    location: {
+      type: {
+        type: { type: String, required: true },
+        coordinates: { type: [Number], required: true },
+      },
+      required: true,
+    },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  },
+  { timestamps: true },
+)
+
+export const Drop = model<DropType>('Drop', DropSchema)
