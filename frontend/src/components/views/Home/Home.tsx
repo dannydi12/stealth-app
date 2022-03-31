ef2fde97d1fa229b75cfa991f4d1ac376449c7d5
pauline-ann <paulinebantayan@gmail.com>
2022-03-31 15:44:45 -0700

    change route to Map

--- frontend/src/components/views/Home/Home.tsx
+++ frontend/src/components/views/Home/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import { Map } from '../../Map'
 import { auth } from '../../routes'
+import { Map } from '../Map'
 
 const Container = styled.div`
    width: 100%;
