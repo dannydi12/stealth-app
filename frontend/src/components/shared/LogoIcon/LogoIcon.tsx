a7975074621330fb786371795a34922613b620c6
Jamel Hammoud <devjamel@gmail.com>
2022-04-01 20:33:57 -0700

    Added new logo

--- frontend/src/components/shared/LogoIcon/LogoIcon.tsx
+++ frontend/src/components/shared/LogoIcon/LogoIcon.tsx
@@ -4,13 +4,7 @@ import { StyledLogoIcon } from '.'
 const LogoIcon: FC = () =>
   (
     <StyledLogoIcon className="logo-icon">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 189.11 189.11">
-        <rect className="cls-1" width="189.11" height="189.11" rx="44.32" />
-        <rect className="cls-2" x="1255.31" y="-307.43" width="10.79" height="113.89" rx="5.4" transform="translate(1303.75 -155.93) rotate(180)" />
-        <rect className="cls-2" x="1358.35" y="-307.43" width="10.79" height="113.89" rx="5.4" transform="translate(1509.81 -155.93) rotate(180)" />
-        <rect className="cls-2" x="1289.66" y="-307.43" width="10.79" height="113.89" rx="5.4" transform="translate(1372.44 -155.93) rotate(180)" />
-        <rect className="cls-2" x="1324" y="-307.43" width="10.79" height="113.89" rx="5.4" transform="translate(1441.13 -155.93) rotate(180)" />
-      </svg>
+      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 826.97 826.97"><rect className="cls-1" width="826.97" height="826.97" rx="193.82" /><path className="cls-2" d="M1159,391.33l-57.1,57.1a14,14,0,0,1-19.81,0L1025,391.33a94.77,94.77,0,1,1,134,0Z" transform="translate(-672.53 -126.51)" /><rect className="cls-3" x="838.83" y="539.25" width="213.37" height="50.9" rx="25.45" transform="translate(-794.9 707.46) rotate(-45)" /><rect className="cls-3" x="742.86" y="443.28" width="213.37" height="50.9" rx="25.45" transform="translate(-755.14 611.49) rotate(-45)" /><rect className="cls-3" x="896.49" y="542.7" width="475.04" height="50.9" rx="25.45" transform="translate(-742.13 841.75) rotate(-45)" /><rect className="cls-3" x="992.46" y="638.67" width="475.04" height="50.9" rx="25.45" transform="translate(-781.88 937.73) rotate(-45)" /></svg>
     </StyledLogoIcon>
   )
 
