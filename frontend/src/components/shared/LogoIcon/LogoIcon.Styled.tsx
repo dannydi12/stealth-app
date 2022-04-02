36b5324cfb0acd2169168f88a1499663148e11fc
Jamel Hammoud <devjamel@gmail.com>
2022-04-01 20:34:07 -0700

    Logo styles

--- frontend/src/components/shared/LogoIcon/LogoIcon.Styled.tsx
+++ frontend/src/components/shared/LogoIcon/LogoIcon.Styled.tsx
@@ -1,12 +1,7 @@
 import styled from 'styled-components'
 
 const StyledLogoIcon = styled.div`
-  .cls-1{
-    fill: #171719;
-  }
-  .cls-2{
-    fill: #fff;
-  }
+.cls-1{fill:#171719;}.cls-2{fill:#007aff;}.cls-3{fill:#fff;}
 `
 
 export default StyledLogoIcon
